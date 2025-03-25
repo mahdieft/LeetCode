5ae87de4037dad7cf64ef1baccd9a3dd9ef454e3
Mahdi Eftekhari Moghaddam <mahdi.eft@gmail.com>
2025-03-25 15:28:01 -0400

    Explore - The LeetCode Beginner's Guide - Solving Your First Problem - Add Two Integers

---
+++ Explore/The LeetCode Beginner's Guide/01. Solving Your First Problem/01. Add Two Integers/add_two_integers.php
@@ -0,0 +1,14 @@
+<?php
+
+class Solution
+{
+    /**
+     * @param Integer $num1
+     * @param Integer $num2
+     * @return Integer
+     */
+    function sum(int $num1, int $num2): int
+    {
+        return $num1 + $num2;
+    }
+}
