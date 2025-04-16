d0779ae54e39d15e0668d4e1163af13336a770ee
Mahdi Eftekhari Moghaddam <mahdi.eft@gmail.com>
2025-04-16 10:08:26 -0400

    Explore - Interview - LeetCode's Interview Crash Course - Linked Lists - Kth node from the end

---
+++ Explore/Interview/LeetCode's Interview Crash Course/Data Structures and Algorithms/03. Linked Lists/02.k_th_node_from_end.php
@@ -0,0 +1,22 @@
+<?php
+
+/*
+ * Given the head of a linked list and an integer k, return the kth node from the end.
+ * For example, given the linked list that represents 1 -> 2 -> 3 -> 4 -> 5 and k = 2,
+ * return the node with value 4, as it is the 2nd node from the end.
+ */
+function findNode($head, $k)
+{
+    $slow = $head;
+    $fast = $head;
+    for ($i = 0; $i < $k; $i++) {
+        $fast = $fast->next;
+    }
+
+    while ($fast) {
+        $slow = $slow->next;
+        $fast = $fast->next;
+    }
+
+    return $slow;
+}
