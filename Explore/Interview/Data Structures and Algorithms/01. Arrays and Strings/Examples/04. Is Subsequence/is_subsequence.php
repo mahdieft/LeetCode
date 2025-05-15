0adf2467a341c94d0b9114337c2160e26c2ebe5b
Mahdi Eftekhari Moghaddam <mahdi.eft@gmail.com>
2025-05-15 18:48:07 -0400

    Explore - Interview - Data Structures and Algorithms - Arrays and Strings - Is Subsequence (PHP)

---
+++ Explore/Interview/Data Structures and Algorithms/01. Arrays and Strings/Examples/04. Is Subsequence/is_subsequence.php
@@ -0,0 +1,17 @@
+<?php
+
+function isSubsequence(string $s, string $t): bool
+{
+    $strlen_s = strlen($s);
+    $strlen_t = strlen($t);
+
+    $pointer_s = $pointer_t = 0;
+    while ($pointer_s < $strlen_s && $pointer_t < $strlen_t) {
+        if ($s[$pointer_s] === $t[$pointer_t]) {
+            $pointer_s++;
+        }
+        $pointer_t++;
+    }
+
+    return $pointer_s === $strlen_s;
+}
