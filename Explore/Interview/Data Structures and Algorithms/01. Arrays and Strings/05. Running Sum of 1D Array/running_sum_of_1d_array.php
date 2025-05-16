0a345448312efae834395fef6532fd578ff25340
Mahdi Eftekhari Moghaddam <mahdi.eft@gmail.com>
2025-05-16 15:14:25 -0400

    Explore - Interview - Data Structures and Algorithms - Arrays and Strings - Running Sum of 1D Array (PHP)

---
+++ Explore/Interview/Data Structures and Algorithms/01. Arrays and Strings/05. Running Sum of 1D Array/running_sum_of_1d_array.php
@@ -0,0 +1,14 @@
+<?php
+
+class Solution
+{
+    function runningSum(array $nums): array
+    {
+        $count_nums = count($nums);
+        for ($i = 1; $i < $count_nums; $i++) {
+            $nums[$i] += $nums[$i - 1];
+        }
+
+        return $nums;
+    }
+}
